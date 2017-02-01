b6f74c78739e201424eae8d9b032b15d8ae4ae70
Alexander Erlandsson <alexander.erlandsson@icloud.com>
2017-02-01 15:32:38 +0100

    Optimised javascript

--- index.js
+++ index.js
@@ -1,55 +1,69 @@
-// Settings
-var input = 'svg/';
-var output = 'css/';
+function svgCssIcons() {
 
-// Base CSS
-var baseCss =
-  '[class^="icon-"], ' +
-  '\r\n[class*=" icon-"] {' +
-    '\r\n\tdisplay: inline-block;' +
-    '\r\n\tvertical-align: middle;' +
-    '\r\n\twidth: 1em;' +
-    '\r\n\theight: 1em;' +
-    '\r\n\tbackground-position: 50% 50%;' +
-    '\r\n\tbackground-repeat: no-repeat;' +
-  '\r\n}\r\n\r\n'
-;
-var iconCss = '';
+  // Settings
+  var input = 'svg/';
+  var output = 'css/';
 
-function createSvgCss(className, svgData) {
-  iconCss +=
-    '.icon-' + className.replace(/\.[^/.]+$/, "") + ' {' +
-    '\r\n\tbackground-image: url(data:image/svg+xml;base64,' + svgData + ');' +
-    '\r\n}\r\n'
+  // Base css
+  var baseCss =
+    '[class^="icon-"], ' +
+    '\r\n[class*=" icon-"] {' +
+      '\r\n\tdisplay: inline-block;' +
+      '\r\n\tvertical-align: middle;' +
+      '\r\n\twidth: 1em;' +
+      '\r\n\theight: 1em;' +
+      '\r\n\tbackground-position: 50% 50%;' +
+      '\r\n\tbackground-repeat: no-repeat;' +
+    '\r\n}\r\n\r\n'
   ;
-}
 
-function createCss() {
-  // Create file
-  fs.writeFile(output + 'svg-icons.css', baseCss + iconCss, function (err) {
-    if (err) return console.log(err);
-    console.log(output + 'svg-icon.css successfully created.');
-  });
-}
+  function createSvgCss(className, svgData) {
+    // Create css class for each icon
+    baseCss +=
+      '.icon-' + className.replace(/\.[^/.]+$/, "") + ' {' +
+      '\r\n\tbackground-image: url(data:image/svg+xml;base64,' + svgData + ');' +
+      '\r\n}\r\n'
+    ;
+  }
 
-function getSvgSource(item, index) {
-  fs.readFile(input + item, 'base64', function (err, data) {
-    if (err) {
-      return console.log(err);
+  function createCss() {
+    // Writes the output to a css file
+    fs.writeFile(output + 'svg-icons.css', baseCss, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(output + 'svg-icon.css successfully created.');
+    });
+  }
+
+  function getSvgSource(item, index) {
+    // Only create icon class if it is a svg
+    var extension = item.substr(item.lastIndexOf('.') + 1);
+
+    if (extension === 'svg') {
+      // Get svg source code and encode it to base64
+      fs.readFile(input + item, 'base64', function (err, data) {
+        if (err) {
+          return console.log(err);
+        }
+
+        createSvgCss(item, data);
+
+        itemsProcessed++;
+        if (itemsProcessed === svgFiles.length) {
+          // Create css file when all files har been looped
+          createCss();
+        }
+      });
     }
-    // Create SVG class
-    // TODO: Only do this is it is a SVG file
-    createSvgCss(item, data);
-    itemsProcessed++;
-    if(itemsProcessed === svgFiles.length) {
-      createCss();
-    }
-  });
+  }
+
+  fs = require('fs');
+
+  // Loop through svg files
+  var svgFiles = fs.readdirSync(input);
+  var itemsProcessed = 0;
+  svgFiles.forEach(getSvgSource);
+
 }
-
-fs = require('fs');
-
-// Loop through SVG files
-var svgFiles = fs.readdirSync(input);
-var itemsProcessed = 0;
-svgFiles.forEach(getSvgSource);
+svgCssIcons();
